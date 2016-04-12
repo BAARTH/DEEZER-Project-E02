95b63ee1d66bacc75d3c66178cc601968440d186
Célia Dagorne <celia.dagorne@hotmail.fr>
2016-04-12 19:40:15 +0200

    Page Nouveautés
    Reste à faire : slides + anims (hover....)

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,6 @@ $(document).ready(function () {
 
 
 	$("#user-interface").hide();
-
-
-
 	$("#user-button").click(
 		function () {
 			$("#user-interface").slideToggle();
@@ -12,7 +9,7 @@ $(document).ready(function () {
 
 		});
 	var boxWidth = $("#right-bar").width();
-	$("#right-bar").width(0);
+	//$("#right-bar").width(0);
 
 
 
@@ -32,6 +29,42 @@ $(document).ready(function () {
 			});
 		}
 	});
+	audiojs.events.ready(function () {
+		var as = audiojs.createAll();
+	});
 
+	(function ($) {
+		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+
+		$('.tab ul.tabs li a').click(function (g) {
+			var tab = $(this).closest('.tab'),
+				index = $(this).closest('li').index();
+
+			tab.find('ul.tabs > li').removeClass('current');
+			$(this).closest('li').addClass('current');
+
+			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+
+			g.preventDefault();
+		});
+	})(jQuery);
+	
+	$(function() {
+  $('.acc_ctrl').on('click', function(e) {
+    e.preventDefault();
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+      $(this).next()
+      .stop()
+      .slideUp(300);
+    } else {
+      $(this).addClass('active');
+      $(this).next()
+      .stop()
+      .slideDown(300);
+    }
+  });
+});
 
 });
